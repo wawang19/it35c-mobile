e0c84df38c896d6b2603f9603cd74d3f6cbc30a6
Wawang19 <ellevera.oscarjay19@gmail.com>
2026-03-26 15:33:19 +0800

    Initial Component

--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,12 +1,23 @@
 import './ExploreContainer.css';
+import { useState } from 'react';
+import { IonButton, IonAlert } from '@ionic/react';
 
 interface ContainerProps { }
 
 const ExploreContainer: React.FC<ContainerProps> = () => {
+  const [isAlertOpen, setIsAlertOpen] = useState(false);
+
   return (
     <div id="container">
-      <strong>Ready to create an app?</strong>
-      <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <IonButton onClick={() => setIsAlertOpen(true)}>Click Me</IonButton>
+      <IonAlert
+        isOpen={isAlertOpen}
+        header="A Short Title Is Best"
+        subHeader="A Sub Header Is Optional"
+        message="A message should be a short, complete sentence."
+        buttons={[{ text: 'OK', handler: () => setIsAlertOpen(false) }]}
+        onDidDismiss={() => setIsAlertOpen(false)}
+      />
     </div>
   );
 };
